f33bc2e4bd2507b3e8cd814421247ba35bbd3a7e
Amirkami33 <105497438+Amirkami33@users.noreply.github.com>
2026-08-05 12:11:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function sendMessage(){
 
+    let input = document.getElementById("userInput");
+    let chat = document.getElementById("chatBox");
+
+    let text = input.value;
+
+    if(text.trim() === ""){
+        return;
+    }
+
+    chat.innerHTML += `
+    <div class="message user">
+    ${text}
+    </div>
+    `;
+
+
+    setTimeout(()=>{
+
+        chat.innerHTML += `
+        <div class="message ai">
+        🐉 دریافت شد. هنوز به مغز اصلی وصل نشدم، ولی آماده‌ام.
+        </div>
+        `;
+
+        chat.scrollTop = chat.scrollHeight;
+
+    },700);
+
+
+    input.value="";
+
+}
